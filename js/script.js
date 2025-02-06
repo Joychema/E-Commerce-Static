165ad5ce446bf9d75b3c17542a9f86691b00b545
Joychema <joymelanin19@gmail.com>
2025-02-06 16:26:48 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// function toggleTheme() {
+//     const body = document.body;
+//     body.classList.toggle("dark-mode");
+
+//     // Save theme preference to local storage
+//     if (body.classList.contains("dark-mode")) {
+//         localStorage.setItem("theme", "dark");
+//     } else {
+//         localStorage.setItem("theme", "light");
+//     }
+// }
+
+// Load saved theme preference on page load
+// window.onload = function () {
+//     const savedTheme = localStorage.getItem("theme");
+//     if (savedTheme === "dark") {
+//         document.body.classList.add("dark-mode");
+//     }
+// };
